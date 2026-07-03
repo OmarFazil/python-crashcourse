ffb13c862c0f880bc6f512c3106a3457a2f6fc7e
Omar Fazil <omarfazil479@gmail.com>
2026-07-03 19:26:01 -0400

    Added Chapter 1
    Made print functions

---
+++ Ch1.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("This is a string")
+print('This is also a string')
+print('This will cause an error') #Error Report says that it is a unterminated string literal (Now fixed)
+#Due to the Error, python stopped everything else from running, preventing a "snowball effect" that can
+# corrupt or overwrite files.
+
+print("This string is to test the VCS known as Git")
+
+#This marks the end of chapter 1
