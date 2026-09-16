d846a294da867502758ebb84d235a01d4c939b9d
Omar Fazil <omarfazil479@gmail.com>
2026-09-16 17:42:05 -0400

    Chapter 2 Partially Added

---
+++ Ch2.py
@@ -0,0 +1,45 @@
+'''
+message = "This is a message! It's a string"
+print(message)
+
+message = "This message includes a random name: Earl"
+print(message)
+'''
+
+# My example:
+#secretMessage = "This is a Secret message for someone"
+#print(secretMessage)
+'''
+messagePlus = "This is a message. It's a little longer than the previous message."
+print(messagePlus)
+
+messagePlus = "Hello"
+print(messagePlus)
+'''
+
+#name = "earl peral"
+#print(name.title())
+#print(name.upper())
+#print(name.lower())
+'''
+first_Name = "earl"
+last_Name = "peral"
+full_Name = f"{first_Name} {last_Name}"
+print(full_Name)
+
+print(f"Leave me alone!, {full_Name.upper()}!")
+'''
+
+#print('Python')
+#print('\tPython')
+
+#print("Transformer Movies: \n\tTransformers\n\tTransformers: Revenge of the Fallen\n\tTransformers: Dark of the Moon")
+
+#pL = '    python    '
+#print(pL)
+#print(pL.strip())
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url)
+
+print(nostarch_url.removeprefix('https://'))
